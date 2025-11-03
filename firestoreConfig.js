4b49b1f0b5cb3e39b6151903695f7c6b9531c0aa
B00145987 Ada Okike <B00145987@mytudublin.ie>
2025-11-03 16:56:53 +0000

    Completed all steps

--- firestoreConfig.js
+++ firestoreConfig.js
@@ -2,6 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
+  "projectId": "casey123",
+  "appId": "1:606492569757:web:326e0a4da32e12782ce474",
+  "storageBucket": "casey123.firebasestorage.app",
+  "apiKey": "AIzaSyBe5EM1qNQtGBazoHN5ywx48plIA81yVcY",
+  "authDomain": "casey123.firebaseapp.com",
+  "messagingSenderId": "606492569757",
+  "projectNumber": "606492569757",
+  "version": "2"
   // Your Firebase configuration object goes here
 }
 
